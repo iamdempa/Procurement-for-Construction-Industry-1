3292024fd31d2aa3870afd627d5f00169a93f1ef
Hewage, Asiri <asiri.hewage@pearson.com>
2019-10-05 23:56:57 +0530

    UI Changes

---
+++ Server/ExpressVendors/testSuit/app.test.js
@@ -0,0 +1,20 @@
+const code = require('./code');
+
+/*
+    test APIs
+ */
+test('Test function Get All Items', () => {
+    expect(code(1, 2)).toBe(3);
+});
+
+test('Test function Get All Items', () => {
+    expect(code(1, 2)).toBe(3);
+});
+
+test('Test function Get All Items', () => {
+    expect(code(1, 6)).toBe(3);
+});
+
+test('Test function Get All Items', () => {
+    expect(code(1, 3)).toBe(3);
+});
