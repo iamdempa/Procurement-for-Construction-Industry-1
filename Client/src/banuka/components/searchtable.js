669b0764c98fde16281578d8c004b4d0758aeedb
iamdempa <jbjayarathna@gmail.com>
2019-10-05 22:17:28 +0530

    Search Table

--- Client/src/banuka/components/searchtable.js
+++ Client/src/banuka/components/searchtable.js
@@ -1,96 +1,116 @@
-import React from 'react';
-import { MDBDataTable } from 'mdbreact';
+import React, { Component } from "react";
+import { MDBDataTable, MDBBtn } from "mdbreact";
+import axios from "axios";
 
-const DatatablePage = () => {
-  const data = {
-    columns: [
-      {
-        label: 'InvoiceID',
-        field: 'InvoiceID',
-        sort: 'asc',
-        width: 150
-      },
-      {
-        label: 'Vendor Name',
-        field: 'Vendor Name',
-        sort: 'asc',
-        width: 270
-      },
-      {
-        label: 'Invoice Date',
-        field: 'Invoice Date',
-        sort: 'asc',
-        width: 200
-      },
-      {
-        label: 'Expected Delivery Date',
-        field: 'Expected Delivery Date',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Billing Address',
-        field: 'Billing Address',
-        sort: 'asc',
-        width: 150
-      },
-      {
-        label: 'Contact Person',
-        field: 'Contact Person',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Total Price',
-        field: 'Total Price',
-        sort: 'asc',
-        width: 100
-      }
-    ],
-    rows: [
-      {
-        name: 'Tiger Nixon',
-        position: 'System Architect',
-        office: 'Edinburgh',
-        age: '61',
-        date: '2011/04/25',
-        salary: '$320'
-      },
-      {
-        name: 'Garrett Winters',
-        position: 'Accountant',
-        office: 'Tokyo',
-        age: '63',
-        date: '2011/07/25',
-        salary: '$170'
-      },
-      {
-        name: 'Ashton Cox',
-        position: 'Junior Technical Author',
-        office: 'San Francisco',
-        age: '66',
-        date: '2009/01/12',
-        salary: '$86'
-      },
-      {
-        name: 'Cedric Kelly',
-        position: 'Senior Javascript Developer',
-        office: 'Edinburgh',
-        age: '22',
-        date: '2012/03/29',
-        salary: '$433'
-      }
-    ]
-  };
+const TableValues = {
+  columns: [
+    {
+      label: "Item Name",
+      field: "Item Name",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Description",
+      field: "Description",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Item ID",
+      field: "Item ID",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Item Code",
+      field: "Item Code",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Unit Price",
+      field: "Unit Price",
+      sort: "asc",
+      width: 270
+    },
+    {
+      label: "Vencdor ID",
+      field: "Vencdor ID",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Date Added",
+      field: "Date Added",
+      sort: "asc",
+      width: 270
+    }
+  ]
+};
 
-  return (
-    <MDBDataTable
-      striped
-      bordered
-      small
-      data={data}
-    />
-  );
+export default class SearchTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      invoices: [],
+      invoicesDuplicate: [],
+      rows: [{}],
+      dataa: [{}]
+    };
+  }
+
+  componentDidMount() {
+
+
+    const rooows = [
+      {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        contactPerson: "banuka",
+        salary: "$320"
+      },
+      {
+        name: "Garrett Winters",
+        position: "Accountant",
+        office: "Tokyo",
+        age: "63",
+        date: "2011/07/25",
+        ontactPerson: "banuka",
+        salary: "$170"
+      }
+    ];
+
+    axios
+      .get("http://localhost:4005/purchaseinvoices/items")
+      .then(response => {
+        TableValues.rows = response.data.filter((item) => {
+          return item.__v !== 0
+        });
+        console.log(response.data)
+        this.setState({ 
+          invoices: response.data,
+          dataa: TableValues
+         });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+      
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <MDBDataTable striped bordered small data={this.state.dataa} />
+      </div>
+    );
+  }
 }
-
-export default DatatablePage;
